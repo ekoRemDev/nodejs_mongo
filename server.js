bcc039418322655de21bd791642620cf63f20670
Ekrem Moralioglu <ekoremdev@gmail.com>
2022-11-23 01:41:21 +0400

    api routes

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({path:'./config/config.env'})
+dotenv.config({path: './config/config.env'})
 
 const app = express();
 
+
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).send({'success': true, 'msg': 'Show all Bootcamps'});
+});
+
+app.post('/api/v1/bootcamp', (req, res) => {
+    res.status(200).send({'success': true, 'msg': 'Create a new Bootcamp'});
+});
+
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).send({'success': true, 'msg': `Update Bootcamp ${req.params.id}`});
+});
+
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).send({'success': true, 'msg': `Delete Bootcamp ${req.params.id}`});
+});
+
+
 const PORT = process.env.PORT || 6000
 
-app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mpde on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mpde on port ${PORT}`));
