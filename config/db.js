98dbb4ba0710a68aa249859a600f15a3afcea7ec
Ekrem Moralioglu <ekoremdev@gmail.com>
2023-02-02 01:52:31 +0400

    deployment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-    const conn = mongoose.connect('mongodb+srv://ekrem123:ekrem123@devcamper.gi15vak.mongodb.net/devcamper?retryWrites=true&w=majority', {
+    const conn = mongoose.connect('process.env.MONGO_URI', {
         useNewUrlParser:true,
         // useCreateIndex:true,
         // useFindAndModify:false
