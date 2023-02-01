9152e2ffedb6fe9156991ca4c4684127fd098ef2
Ekrem Moralioglu <ekoremdev@gmail.com>
2023-02-02 01:53:55 +0400

    deployment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-    const conn = mongoose.connect('process.env.MONGO_URI', {
+    const conn = mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser:true,
         // useCreateIndex:true,
         // useFindAndModify:false
